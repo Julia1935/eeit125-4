bb959c1ce3315e4092b8ceb391ac19c45c983009
Student <armyant0920@gmail.com>
2020-11-04 16:56:19 +0800

    11/4 test
    update

---
+++ src/Product.java
@@ -0,0 +1,74 @@
+import com.sun.org.apache.xml.internal.security.keys.keyresolver.implementations.PrivateKeyResolver;
+
+public class Product {
+	
+	private int product_Id;
+	private String product_Name;
+	private String produect_Brand;
+	private String product_Type;
+	private String product_Category;
+	private String product_Skin;
+	private String product_Comment;
+	private int product_Price;
+	
+	public int getProduct_Price() {
+		return product_Price;
+	}
+
+	public void setProduct_Price(int product_Price) {
+		this.product_Price = product_Price;
+	}
+
+	public Product() {
+		
+	}
+	
+	
+	public int getProduct_Id() {
+		return product_Id;
+	}
+	public void setProduct_Id(int product_Id) {
+		this.product_Id = product_Id;
+	}
+	public String getProduct_Name() {
+		return product_Name;
+	}
+	public void setProduct_Name(String product_Name) {
+		this.product_Name = product_Name;
+	}
+	public String getProduect_Brand() {
+		return produect_Brand;
+	}
+	public void setProduect_Brand(String produect_Brand) {
+		this.produect_Brand = produect_Brand;
+	}
+	public String getProduct_Type() {
+		return product_Type;
+	}
+	public void setProduct_Type(String product_Type) {
+		this.product_Type = product_Type;
+	}
+	public String getProduct_Category() {
+		return product_Category;
+	}
+	public void setProduct_Category(String product_Category) {
+		this.product_Category = product_Category;
+	}
+	public String getProduct_Skin() {
+		return product_Skin;
+	}
+	public void setProduct_Skin(String product_Skin) {
+		this.product_Skin = product_Skin;
+	}
+	public String getProduct_Comment() {
+		return product_Comment;
+	}
+	public void setProduct_Comment(String product_Comment) {
+		this.product_Comment = product_Comment;
+	}
+	
+	
+	
+	
+
+}
