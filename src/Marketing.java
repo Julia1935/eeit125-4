ebee6a44778ec4de61c315fba04d6e80e91e0715
Mildoto <73803671+Mildoto@users.noreply.github.com>
2020-11-08 15:04:05 +0800

    Revert "Revert "Merge pull request #1 from Chitzuyun/master""
    This reverts commit 92e9a120cc5082ec765ab6c867a5b7d9a7ed9549.

---
+++ src/Marketing.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+
+public class Marketing {
+	
+	private int Marketing_id;
+	private String name;
+	private Date start_date;
+	private Date end_date;
+	private String description;
+	private String Brand_no;
+	private String Brand_name;
+	private int product_Id;
+	private String product_name;
+	private String product_type;
+	private String product_category;
+	private String product_series;
+	private String product_skin;
+	private int product_price;
+	private float product_rating;
+	
+	
+}
